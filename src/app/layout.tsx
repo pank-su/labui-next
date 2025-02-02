967e05d96942443fdc2cdbfc78a21766cdaa5e20
pank-su <frostkslo1@yandex.ru>
2025-02-02 21:45:32 +0300

    ✨ (коллекция) добавлена генерация колонок и корректное отображение данных

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,33 @@
+"use client"
 
 import './global.css'
 import styles from './styles.module.css'
 import Header from './components/header'
 import { AntdRegistry } from '@ant-design/nextjs-registry'
-import { ConfigProvider } from 'antd'
+import { ConfigProvider, theme } from 'antd'
 import ruRU from 'antd/locale/ru_RU';
 import '@ant-design/v5-patch-for-react-19';
+
+
+const { useToken } = theme;
+
 
 export default function Layout({ children }: {
     children: React.ReactNode
 }) {
+
+    const { token } = useToken();
+
+
     return (
         <html lang="ru" suppressHydrationWarning>
             <body>
                 <AntdRegistry>
                     <ConfigProvider locale={ruRU}>
                         <div>
-                            <div className={styles.header}>
+                            <div className={styles.header} style={{
+                                backgroundColor: token.colorBgBase
+                            }}>
                                 <Header />
                             </div>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,20 @@
+"use client"
+
 import { Table } from "antd"
 import { createClient } from "../utils/supabase/client"
 
-const addKey = <T extends {id: number | null }>(obj: T): T & {key: number | null} => {
+const addKey = <T extends { id: number | null }>(obj: T): T & { key: number | null } => {
     return Object.defineProperty(obj, "key", {
         get() {
             return this.id
         },
         enumerable: true,
         configurable: false
-    }) as T & {key: number|null};
+    }) as T & { key: number | null };
 };
 
 
 
-
-async function loadCollection() {
-    const supabase = createClient()
-    const {data} = (await supabase.from("basic_view").select())
-
-    const newData = data?.map((row) => addKey(row))
-
-    const columns = Object.keys(data?.at(0)!)
-    
-   
-
-    
-    
-    return  <Table dataSource={newData} pagination={false}>
-
-    </Table>
-}
 
 
 /**
@@ -37,6 +22,28 @@ async function loadCollection() {
  * 
  */
 export default async function Page() {
-    return await loadCollection()
+    const supabase = createClient()
+    const { data } = (await supabase.from("basic_view").select())
+    // const { width, height } = useWindowSize();
+
+    // Если есть данные, создаём колонки на основе ключей первого объекта
+    const columns = (data ?? []).length > 0
+        ? Object.keys(data![0]).map((key) => ({
+            title: key,      // Заголовок колонки
+            dataIndex: key,  // Поле данных для колонки
+            key: key         // Уникальный ключ колонки
+        }))
+        : []; // TODO: переделать колонки во что-то более адекватное
+
+
+    // Добавляем ключ для каждого ряда
+    const keyedData = data?.map((row) => addKey(row)) ?? [];
+    
+
+
+
+    return <Table virtual size="small" dataSource={keyedData}  pagination={false} columns={columns}>
+
+    </Table>
 }
 
