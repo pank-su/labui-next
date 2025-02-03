3127e3dc111b6dc4c9a5713bff9caec0f97b7c5b
pank-su <frostkslo1@yandex.ru>
2025-02-03 17:50:50 +0300

    💄 добавление индикации входа

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,17 +1,18 @@
-"use client";
-
-import { Avatar } from "antd";
+import { Avatar, Skeleton } from "antd";
 import Navigation from "./navigation";
 import { Search } from "./search";
+import { Suspense, useEffect, useState } from "react";
+import { createClient } from "@/src/utils/supabase/client";
+import AuthBtn from "./auth-btn";
+import { User } from "@supabase/supabase-js";
 
 
 const testImage = "https://images.dog.ceo/breeds/akita/An_Akita_Inu_resting.jpg"
 
 export default function Header() {
     return <div className="w-full h-full px-8 flex justify-between items-center">
-        <Avatar>
-            
-        </Avatar>
+        <AuthOrAvatar />
+
         <Navigation />
         <Search />
         <Avatar.Group size={"small"}>
@@ -22,4 +23,33 @@ export default function Header() {
 
         </Avatar.Group>
     </div>
+}
+
+/**
+ * Кнопка или аватар авторизации
+ */
+function AuthOrAvatar() {
+
+    const [loading, setLoading] = useState(true);
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        async function fetchUser() {
+            const supabase = createClient();
+            const {
+                data: { user },
+            } = await supabase.auth.getUser();
+            setUser(user);
+            setLoading(false);
+        }
+        fetchUser();
+    }, []);
+
+    if (loading) {
+        return <Skeleton.Avatar active />;
+    }
+    if (!user) {
+        return <AuthBtn />;
+    }
+    return <Avatar />
 }
