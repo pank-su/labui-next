ed5f5fa9acc488ae7425a294f124cdd18ddd303b
pank-su <frostkslo1@yandex.ru>
2025-02-03 16:35:43 +0300

    🔥 удаление старой тестовой картинки

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,8 +5,6 @@ import Navigation from "./navigation";
 import { Search } from "./search";
 
 
-const testImage = "https://images.dog.ceo/breeds/akita/An_Akita_Inu_resting.jpg"
-
 export default function Header() {
     return <div className="w-full h-full px-8 flex justify-between items-center">
         <Avatar>
