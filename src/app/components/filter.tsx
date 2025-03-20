05cc50ef08c7959f89a95012e80dd5630b0126b4
pank-su <frostkslo1@yandex.ru>
2025-03-20 22:33:03 +0300

    🩹 (фильтр) теперь при добавлении записи видно новую запись

--- src/app/components/filter.tsx
+++ src/app/components/filter.tsx
@@ -1,12 +1,31 @@
 import { parseIndexFilter } from "@/utils/parseIndexFilter";
 import { Column } from "@tanstack/react-table";
 import { Input } from "antd";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
+/**
+ * Фильтр индекса
+ * 
+ * 
+ * @param column колонку которую необходимо фильтровать
+ * 
+ */
 function IndexFilter({ column }: { column: Column<any, unknown> }) {
     const filterValue = column.getFilterValue()
+    const minMax = column.getFacetedMinMaxValues()
     const [value, setValue] = useState("")
 
+    // Если максимальное значение обновилось
+    useEffect(() => {
+        if (value.trim() === "") {
+            column.setFilterValue(undefined)
+            return
+        }
+        const filter = parseIndexFilter(value, 1, minMax?.[1] ?? 9999)
+        column.setFilterValue(filter)
+    }, [minMax?.[1], value])
+
+    // Если фильтр был сброшен, то очищаем поле
     useEffect(() => {
         if (filterValue == undefined && !column.getIsFiltered()) {
             setValue("")
@@ -14,18 +33,12 @@ function IndexFilter({ column }: { column: Column<any, unknown> }) {
     }, [filterValue])
 
     return (
-        <Input 
-            placeholder="1,3-5" 
-            size="small" 
-            value={value} 
+        <Input
+            placeholder="1,3-5"
+            size="small"
+            value={value}
             onChange={e => {
                 setValue(e.target.value)
-                if (e.target.value.trim() === "") {
-                    column.setFilterValue([-1])
-                    return
-                }
-                const filter = parseIndexFilter(e.target.value, 1, column.getFacetedMinMaxValues()?.[1]!!)
-                column.setFilterValue(filter)
             }}
         />
     )
@@ -33,10 +46,10 @@ function IndexFilter({ column }: { column: Column<any, unknown> }) {
 
 export default function Filter({ column }: { column: Column<any, unknown> }) {
     const { filterVariant } = column.columnDef.meta ?? {}
-    
+
     return (
         <>
-            {filterVariant === "index" ? <IndexFilter column={column} /> : <></>}
+            {filterVariant === "index" && <IndexFilter column={column} />}
         </>
     )
 }
