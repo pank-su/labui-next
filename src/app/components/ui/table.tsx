b073c630a144aabf5d57b62b4ef48183823aa1cd
pank-su <pank@pank.su>
2025-07-09 12:00:25 +0300

    :lipstick: изменён размер текста, для того чтобы все колонки отображались равными

--- src/app/components/ui/table.tsx
+++ src/app/components/ui/table.tsx
@@ -73,7 +73,7 @@ const TableHead = React.forwardRef<
   <th
     ref={ref}
     className={cn(
-      "h-6 px-2 bg-background hover:bg-muted align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] border border-slate-300",
+      "text-base px-2 bg-background hover:bg-muted align-middle font-medium text-muted-foreground [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] border border-slate-300",
       className
     )}
     {...props}
@@ -88,7 +88,7 @@ const TableCell = React.forwardRef<
   <td
     ref={ref}
     className={cn(
-      "px-2 align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] border",
+      "px-2 text-base align-middle [&:has([role=checkbox])]:pr-0 [&>[role=checkbox]]:translate-y-[2px] border",
       className
     )}
     {...props}
