b349666241f7adcb5509b450c49c3f2ea8a87c44
pank-su <pank@pank.su>
2025-07-14 11:50:50 +0300

    :fire: удаление карты (/map)

--- src/app/map/queries.ts
+++
@@ -1,6 +0,0 @@
-import {TypedSupabaseClient} from "@/utils/supabase/typed";
-import {getBasicView} from "@/app/(general)/queries";
-
-export function getPositions(client: TypedSupabaseClient) {
-    return  //(getBasicView(client))
-}
